9a4732c4adf47f753161ec032404611c6e7d2710
yurcifer <yurcifer.dev@gmail.com>
2022-07-27 19:54:13 +0300

    login form

--- client/src/Components/Login/Login.jsx
+++ client/src/Components/Login/Login.jsx
@@ -1,9 +1,34 @@
 import React, { useEffect } from 'react';
-import styles from './Login.module.css';
+import { useState } from 'react';
+import styles from './Login.module.scss';
 
 export default function Login() {
+  const [login, setLogin] = useState('');
+  const [pwd, setPwd] = useState('');
   useEffect(() => {
-    console.log(styles)
-  })
-  return <h1 className={styles.title}>Login</h1>;
+    console.log(styles);
+  });
+
+  function submit(e) {
+    e.preventDefault();
+    console.log(login, pwd);
+    setLogin('');
+    setPwd('');
+  }
+  return (
+    <>
+      <h1 className={styles.title}>Login</h1>
+      <form>
+        <label>
+          Login:
+          <input onChange={(e) => setLogin(e.target.value)} value={login}/>
+        </label>
+        <label>
+          Password:
+          <input onChange={(e) => setPwd(e.target.value)} type="password" value={pwd}/>
+        </label>
+        <button onClick={e => submit(e)}>Login</button>
+      </form>
+    </>
+  );
 }
