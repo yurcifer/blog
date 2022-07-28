f10fdb8822d47f0d01eed9f65546145dc20819d2
yurcifer <yurcifer.dev@gmail.com>
2022-07-28 22:12:06 +0300

    added firebase, handle submit

--- client/src/Components/Login/Login.jsx
+++ client/src/Components/Login/Login.jsx
@@ -1,19 +1,36 @@
 import React, { useEffect } from 'react';
 import { useState } from 'react';
 import styles from './Login.module.scss';
+import { useNavigate } from 'react-router-dom';
+import { signInWithEmailAndPassword } from 'firebase/auth';
 
-export default function Login() {
+export default function Login({ auth }) {
   const [login, setLogin] = useState('');
   const [pwd, setPwd] = useState('');
-  useEffect(() => {
-    console.log(styles);
-  });
+  const navigate = useNavigate();
 
   function submit(e) {
     e.preventDefault();
-    console.log(login, pwd);
     setLogin('');
     setPwd('');
+    signInWithEmailAndPassword(auth, login, pwd)
+      .then(async ({ user }) => {
+        const idToken = await user.getIdToken();
+        await fetch('http://localhost:5000/sessionLogin', {
+          credentials: 'include',
+          method: 'POST',
+          headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ idToken }),
+        });
+        await auth.signOut();
+        navigate('/');
+      })
+      .catch(error => {
+        console.log(error);
+      });
   }
   return (
     <>
@@ -21,11 +38,11 @@ export default function Login() {
       <form>
         <label>
           Login:
-          <input onChange={(e) => setLogin(e.target.value)} value={login}/>
+          <input onChange={e => setLogin(e.target.value)} value={login} />
         </label>
         <label>
           Password:
-          <input onChange={(e) => setPwd(e.target.value)} type="password" value={pwd}/>
+          <input onChange={e => setPwd(e.target.value)} type="password" value={pwd} />
         </label>
         <button onClick={e => submit(e)}>Login</button>
       </form>
--- client/src/index.js
+++ client/src/index.js
@@ -4,13 +4,29 @@ import './index.css';
 import App from './App';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './Components/Login/Login';
+import { initializeApp } from "firebase/app";
+import { getAuth, setPersistence, signInWithEmailAndPassword, browserSessionPersistence } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDkfgvZgIK3cVFGNxBgB2uudggatMsP84Q",
+  authDomain: "blog-e9e07.firebaseapp.com",
+  projectId: "blog-e9e07",
+  storageBucket: "blog-e9e07.appspot.com",
+  messagingSenderId: "661437548761",
+  appId: "1:661437548761:web:b6b3e43b3de7ff5e25d6c8"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+setPersistence(auth, browserSessionPersistence);
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="/login" element={<Login />} />
+      <Route path="/" element={<App app={app}/>} />
+      <Route path="/login" element={<Login auth={auth}/>} />
     </Routes>
   </BrowserRouter>
 );
